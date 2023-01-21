6bc61f6faafcdfc2d0a7eeb2112fdad287f7cd3d
quentinmathieu <quentin.mth@gmail.com>
2023-01-21 13:13:14 +0100

    that's work ! set EXE=csgo.exe was missing

--- dual_screen_batch.bat
+++ dual_screen_batch.bat
@@ -1,10 +1,11 @@
-@echo off
+
 
 @REM switch to only 1 screen
 DisplaySwitch.exe /internal
 
 
-set steamPath = "C:\Program Files (x86)\Steam\steam.exe"
+@REM set steamPath = "C:\Program Files (x86)\Steam\steam.exe"
+@REM set start =  steam://rungameid/730
 @REM set csgopath=steam://rungameid/730
 @REM cd %csgopath%
 @REM 
@@ -16,10 +17,10 @@ set steamPath = "C:\Program Files (x86)\Steam\steam.exe"
 "C:\Program Files (x86)\Steam\steam.exe" steam://rungameid/730
 
 
-timeout /t 60
+timeout /t 30
 
 SETLOCAL EnableExtensions
-
+set EXE=csgo.exe
 :before
 @REM While csgo is running display that it's running ; THEN when you quit csgo leave the for loop
 FOR /F %%x IN ('tasklist /NH /FI "IMAGENAME eq %EXE%"') DO IF %%x == %EXE% (
