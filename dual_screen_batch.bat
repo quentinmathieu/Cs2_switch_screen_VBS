679b15f331bec2aa2e07d6bbd56cbd9315307726
quentinmathieu <quentin.mth@gmail.com>
2023-01-21 13:19:04 +0100

    "wait csgo to lauch" feature added

--- dual_screen_batch.bat
+++ dual_screen_batch.bat
@@ -1,26 +1,24 @@
+@echo off
 
-
-@REM switch to only 1 screen
+@REM switch to only 1st screen
 DisplaySwitch.exe /internal
 
 
-@REM set steamPath = "C:\Program Files (x86)\Steam\steam.exe"
-@REM set start =  steam://rungameid/730
-@REM set csgopath=steam://rungameid/730
-@REM cd %csgopath%
-@REM 
-@REM 
-@REM set EXE=csgo.exe
-@REM 
-@REM start %EXE%
-
+@REM launch CSGO
 "C:\Program Files (x86)\Steam\steam.exe" steam://rungameid/730
 
 
-timeout /t 30
+set EXE=csgo.exe
+:waitingOpen
+@REM Waiting csgo to be launched
+FOR /F %%x IN ('tasklist /NH /FI "IMAGENAME eq %EXE%"') DO IF NOT %%x == %EXE% (
+  echo %EXE% waiting for csgo to launch...
+  timeout /t 1
+  goto :waitingOpen
+)
+
 
 SETLOCAL EnableExtensions
-set EXE=csgo.exe
 :before
 @REM While csgo is running display that it's running ; THEN when you quit csgo leave the for loop
 FOR /F %%x IN ('tasklist /NH /FI "IMAGENAME eq %EXE%"') DO IF %%x == %EXE% (
