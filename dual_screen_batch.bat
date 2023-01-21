514ec06d57699158a9ee3089e6a7591b5af2f4d0
quentinmathieu <quentin.mth@gmail.com>
2023-01-21 13:05:51 +0100

    game lauched fixed;quit game doesnt extend screens

--- dual_screen_batch.bat
+++ dual_screen_batch.bat
@@ -3,19 +3,25 @@
 @REM switch to only 1 screen
 DisplaySwitch.exe /internal
 
-set csgopath=D:\SteamLibrary\steamapps\common\Counter-Strike Global Offensive
-cd %csgopath%
-dir
-pause
 
-@REM cd  D:\SteamLibrary\steamapps\common\Counter-Strike Global Offensive
-@REM echo %cd%
+set steamPath = "C:\Program Files (x86)\Steam\steam.exe"
+@REM set csgopath=steam://rungameid/730
+@REM cd %csgopath%
+@REM 
+@REM 
+@REM set EXE=csgo.exe
+@REM 
+@REM start %EXE%
 
-timeout /t 5
+"C:\Program Files (x86)\Steam\steam.exe" steam://rungameid/730
+
+
+timeout /t 60
 
 SETLOCAL EnableExtensions
-set EXE=csgo.exe
+
 :before
+@REM While csgo is running display that it's running ; THEN when you quit csgo leave the for loop
 FOR /F %%x IN ('tasklist /NH /FI "IMAGENAME eq %EXE%"') DO IF %%x == %EXE% (
   echo %EXE% is Running
   timeout /t 5
@@ -23,6 +29,7 @@ FOR /F %%x IN ('tasklist /NH /FI "IMAGENAME eq %EXE%"') DO IF %%x == %EXE% (
 )
 
 
+@REM that extends screens
 DisplaySwitch.exe /extend
 
 
